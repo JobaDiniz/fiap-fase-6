098a35595f00d3d4c3cb64566c30f752d1d19bcf
JobaDiniz <jobertodinizjunior@gmail.com>
2021-08-18 18:21:46 -0300

    chore: add barebone projects

---
+++ src/Presentation/iFood.Reviews.Api/Controllers/ReviewController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace iFood.Reviews.Api.Controllers
+{
+    [ApiController]
+    [Route("review")]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ILogger<ReviewController> logger;
+
+        public ReviewController(ILogger<ReviewController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetPaged()
+        {
+            return Ok();
+        }
+    }
+}
